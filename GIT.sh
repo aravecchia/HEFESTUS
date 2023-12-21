508c3e530fe0e4cf32ff7b4eb2f098bc8cd4da49
Alexandre <aravecchia@gmail.com>
2023-12-21 04:29:07 -0300

    Adicionar toda a pasta SCRIPTS-GIT e seu conteúdo

---
+++ GIT.sh
@@ -0,0 +1,8 @@
+# Adicione toda a pasta SCRIPTS-GIT ao staging area
+git add SCRIPTS-GIT/
+
+# Faça um commit com as alterações adicionadas
+git commit -m "Adicionar pasta SCRIPTS-GIT"
+
+# Faça o push para o repositório remoto (GitHub)
+git push origin master
