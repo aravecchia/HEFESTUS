d65e6b1052e2d2835cbae139efc5e7ac072be662
Alexandre <aravecchia@gmail.com>
2023-12-24 17:11:16 -0300

    Mensagem do commit

--- SCRIPTS-GIT/atualizar-repo-hefestus.sh
+++ SCRIPTS-GIT/atualizar-repo-hefestus.sh
@@ -1,19 +1,36 @@
 #!/bin/bash
 
+
 # Vá para o diretório do seu repositório
 cd /home/alexandre/HEFESTUS/SCRIPTS-GIT
 
-# Fazer um commit com as alterações
-git commit -m "Atualização"
+# Certifique-se de estar no ramo correto
+git checkout main
 
-# Fazer um commit com as alterações
-git commit -a
-
-# Atualize o repositório local
+# Faça um pull para obter as alterações mais recentes do repositório remoto
 git pull origin main
 
-# Push para o repositório remoto
+# Faça um push para enviar as alterações locais para o repositório remoto (GitHub)
 git push origin main
+
+
+
+
+
+
+
+
+# Fazer um commit com as alterações
+#git commit -m "Atualização"
+
+# Fazer um commit com as alterações
+#git commit -a
+
+# Atualize o repositório local
+#git pull origin main
+
+# Push para o repositório remoto
+#git push origin main
 
 # Se necessário, execute outras etapas de atualização aqui
 cd 
